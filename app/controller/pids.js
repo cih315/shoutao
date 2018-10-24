92c748b835f36aea80748acd9a10ad0d57604e8f
root <root@ALS-20161123JFL.localdomain>
2018-10-24 18:16:32 +0800

    update

--- app/controller/pids.js
+++ app/controller/pids.js
@@ -22,6 +22,14 @@ const pids = {
     session: '7000010142156277b007a9d4746a5ea9911e21a5c13626f4e238ea754331766154d5397267987083',
     tklImg: 'https://gw.a-li-cdn.com/tfs/TB1c.wHdh6I8KJjy0FgXXXXzVXa-580-327.png',
     tklTitle: '选客家福利购'
+  },
+  "14942785-148850310-47758250221": { //王川
+    suffix: "/14942785-148850310-47758250221",
+    home: "/14942785-148850310-47758250221",
+    pid: "mm_14942785_148850310_47758250221",
+    session: '7000010142156277b007a9d4746a5ea9911e21a5c13626f4e238ea754331766154d5397267987083',
+    tklImg: 'https://gw.a-li-cdn.com/tfs/TB1c.wHdh6I8KJjy0FgXXXXzVXa-580-327.png',
+    tklTitle: '选客家福利购'
   }
 }
 
