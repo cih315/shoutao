4b019d2103227eb6e31d14347db7dd0d87526585
root <root@lowang.localdomain>
2018-11-05 20:06:24 +0800

    update

--- spider_duodian.js
+++ spider_duodian.js
@@ -10,12 +10,12 @@ const h5coupon = require('./h5coupon')
 const pid = 'mm_14942785_97600036_18176850324';
 const session = '7000010142156277b007a9d4746a5ea9911e21a5c13626f4e238ea754331766154d5397267987083';
 
-const date = '20181102'
+const date = '20181105'
 const prefix = date + '-1'
 const outputBase = __dirname + '/dd/' + date + '/'
 const tmpBase = __dirname + '/tmp'
 const htmlFile = outputBase + prefix + '.html'
-const mp_link = 'https://mp.weixin.qq.com/s/Bgtna1Q-8QWyWzYYIoHzhw'
+const mp_link = 'https://mp.weixin.qq.com/s/KjUtKvr7Ku1HxPVZYUgz_A'
 const len = 100
 
 async function getMpHtml(url) {
