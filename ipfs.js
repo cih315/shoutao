c6239a8776564a3f8bf363062d3d386b2f98e671
root <root@ALS-20161123JFL.localdomain>
2018-11-27 11:42:11 +0800

    update

--- ipfs.js
+++ ipfs.js
@@ -2,7 +2,7 @@ const ipfsAPI = require('ipfs-api')
 const ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5001')
 
 //console.log(ipfs)
-ipfs.util.addFromFs('/root/shoutao/dd/20180927', { recursive: true, ignore: [] }, (err, result) => {
+ipfs.util.addFromFs('/root/shoutao/dd/20181127', { recursive: true, ignore: [] }, (err, result) => {
   if (err) { throw err }
   console.log(result)
 });
