3aaf61d6da5894b310a887f4f93227950c43e841
root <root@ALS-20161123JFL.localdomain>
2018-12-20 14:49:42 +0800

    update

--- spider_duodian.js
+++ spider_duodian.js
@@ -14,12 +14,12 @@ const h5coupon = require('./h5coupon')
 const pid = 'mm_14942785_97600036_18176850324';
 const session = '70000100d124971b34ec9ece19c8fafc3f889c12850cafea0300ac192b4dbfb78d0103d267987083';
 
-const date = '20181219'
+const date = '20181220'
 const prefix = date + '-1'
 const outputBase = __dirname + '/dd/' + date + '/'
 const tmpBase = __dirname + '/tmp'
 const htmlFile = outputBase + prefix + '.html'
-const mp_link = 'https://mp.weixin.qq.com/s/GPynoiNr0uFe1z2qDwjrJw'
+const mp_link = 'https://mp.weixin.qq.com/s/9Pm5FANawq1yNSJDjH74JA'
 const len = 100
 
 async function getMpHtml(url) {
