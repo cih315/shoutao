109b00a378c39194fc16d56363af13ba39e44a4c
Wang.ch <Wang.ch@gaiasys.cn>
2018-12-21 14:18:31 +0800

    青桔单车卡活动

--- spider_duodian.js
+++ spider_duodian.js
@@ -14,12 +14,12 @@ const h5coupon = require('./h5coupon')
 const pid = 'mm_14942785_97600036_18176850324';
 const session = '70000100d124971b34ec9ece19c8fafc3f889c12850cafea0300ac192b4dbfb78d0103d267987083';
 
-const date = '20181220'
+const date = '20181221'
 const prefix = date + '-1'
 const outputBase = __dirname + '/dd/' + date + '/'
 const tmpBase = __dirname + '/tmp'
 const htmlFile = outputBase + prefix + '.html'
-const mp_link = 'https://mp.weixin.qq.com/s/9Pm5FANawq1yNSJDjH74JA'
+const mp_link = 'https://mp.weixin.qq.com/s/V-a5KVIPJ6AszZ-fHzQu5g'
 const len = 100
 
 async function getMpHtml(url) {
